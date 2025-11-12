ff54ae1a103584ff07fb6e0e13ed6bb51b0010cb
Bilboow <93973179+Bilboow@users.noreply.github.com>
2025-11-12 22:04:54 +0530

    Update import for RetrievalQA to new path

--- helper.py
+++ helper.py
@@ -4,7 +4,7 @@ import pandas as pd
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA 
+from langchain.chains.retrieval_qa.base import RetrievalQA  # ✅ use this
 from langchain_core.documents import Document
 from langchain_community.document_loaders import CSVLoader
 from langchain.prompts import PromptTemplate
